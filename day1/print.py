df9374adf3994032493fb79cebb41a4b9a1d3664
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2025-03-29 16:16:12 +0100

    day1 finished

--- day1/print.py
+++ day1/print.py
@@ -10,3 +10,12 @@ username=input("What is your name?")
 lenght=len(username)
 print("Number of characters: ",lenght)
 
+# Create a greeting for your program.
+# Ask the user for the city that they grew up in and store it in a variable.
+# Ask the user for the name of a pet and store it in a variable.
+# Combine the name of their city and pet and show them their band name.
+
+print("Band Name Generator project ")
+cityName = input("Which city did you grow up in\n?")
+petName = input("What is the name of a pet\n?")
+print("Your band name could be " + cityName + " " + petName)

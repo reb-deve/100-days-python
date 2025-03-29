730e8ebaa6110398d875c471c878b881d4a08193
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2025-03-29 16:09:18 +0100

    day1

---
+++ day1/print.py
@@ -0,0 +1,12 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# print(len(input("What is your name?")))
+
+username=input("What is your name?")
+lenght=len(username)
+print("Number of characters: ",lenght)
+

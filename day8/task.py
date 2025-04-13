f59b5dc5075b472792129a35d3eb0b8b1b61f46f
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2025-04-13 23:31:41 +0200

    caesar

--- day8/task.py
+++ day8/task.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+""" alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 
@@ -15,7 +15,7 @@ def encrypt(plain_text, shift_amount):
         else:
             result += letter
     print(f"Encrypted message: {result}")
-    
+
 def decrypt(cipher_text, shift_amount):
 	result = ""
 	for letter in cipher_text:
@@ -45,4 +45,40 @@ else:
     print(f"Wrong direction entered: {direction}")
 # TODO-5: Create a function called 'decrypt()' that takes 'cipher_text' and 'shift_amount' as inputs.
 # TODO-6: Inside the 'decrypt()' function, shift the letters back to their original positions.
+
+ """
+
+ #alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+def caesar(original_text, shift_amount, encode_or_decode):
+    alphabet = [chr(i)for i in range(97,123)]
+    result = ""
+    if encode_or_decode == "decode":
+        shift_amount*=-1
+    for char in original_text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_index=(index+shift_amount) % len(alphabet)
+            result += alphabet[new_index]
+        else:
+            result += char
+    print(f"Here is the {encode_or_decode} result: {result}")
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+
+
+
+
+
 
